52a475debbd668d97eb163485a18b421d3a66bd6
BeneArp <benedetta.arpino@gmail.com>
2024-10-01 19:11:44 +0200

    feat: add technologies and types output

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -8,16 +8,16 @@
 
       return{
         projects:[],
-
+        technologies:[],
+        types:[],
       }
 
     },
 
     methods:{
-      getApi(){
+      getProjects(){
 
         // console.log('sto funzionando');
-
 
         store.getData.getRequest(store.apiUrl + 'progetti')
           .then(res => {  
@@ -29,11 +29,40 @@
             
         });
       
-      }
+      },
+
+      getTechnologies(){
+
+        store.getData.getRequest(store.apiUrl + 'tecnologie')
+          .then(res =>{
+            this.technologies = res.data;
+            console.log(this.technologies);
+          })
+          .catch(err => {
+            console.log(err);
+            
+          })
+      },
+
+      getTypes(){
+        store.getData.getRequest(store.apiUrl + 'tipi')
+          .then(res => {
+            this.types = res.data;
+            console.log( this.types);
+          })
+          .catch(err => {
+            console.log(err);
+            
+          })
+      },
     },
 
     mounted(){
-      this.getApi();
+      this.getProjects();
+
+      this.getTechnologies();
+
+      this.getTypes();
     }
   }
 </script>
@@ -42,16 +71,53 @@
 
   <h1>Progetti</h1>
 
-  <div>
+  <div class="wrapper">
 
-    <ul>
-      <li v-for="project in projects"> <strong>Id:</strong> {{ project.id }} | <strong>Titolo:</strong> {{ project.title }} | <strong>Data d'inizio:</strong> {{  project.start_date  }} | <strong>Data fine:</strong> {{ project.end_date }}</li>
-    </ul>
+    <!-- elenco progetti -->
+    <div>
+
+      <h3>Lista progetti</h3>
+
+      <ul>
+        <li v-for="project in projects"> <strong>Id:</strong> {{ project.id }} | <strong>Titolo:</strong> {{ project.title }} | <strong>Data d'inizio:</strong> {{  project.start_date  }} | <strong>Data fine:</strong> {{ project.end_date }}</li>
+      </ul>
+
+    </div>
+
+    <!-- elenco tecnologie e categorie -->
+    <div>
+
+      <h3>Tecnologie</h3>
+
+      <ul>
+        <li v-for="technology in technologies"><strong>Nome:</strong> {{ technology.name }} </li>
+      </ul>
+
+
+      <h3>Tipologie</h3>
+
+      <ul>
+        <li v-for="type in types"> <strong>Nome:</strong> {{ type.name }} </li>
+      </ul>
+
+    </div>
 
   </div>
 
+
+
 </template>
 
-<style>
+<style lang="scss" scoped>
+
+  .wrapper{
+    display: flex;
+    justify-content: space-between;
+    gap: 5em;
+  }
+
+  h3{
+    margin-top: 2em;
+  }
 
 </style>
